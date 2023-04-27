0470563d232f1b26d1cd282bcdc7a78623c77369
Samuel Handel <sahandel@wisc.edu>
2023-04-27 10:45:05 -0500

    updating to run for 1 file

--- project.sh
+++ project.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-wget 
+#wget $1 --no-check-certificate
+
+#rscript proj.R $1 
+if wget $1 --no-check-certificate; then
+    echo "wget was successful"
+else
+    echo "wget failed"
+fi
