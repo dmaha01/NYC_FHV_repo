9dd720a66f10a56cb3b25606d7bc4c74c06ab47e
Samuel Handel <sahandel@wisc.edu>
2023-05-02 18:05:11 -0500

    updating

--- project.sh
+++ project.sh
@@ -3,8 +3,18 @@
 #wget $1 --no-check-certificate
 
 #rscript proj.R $1 
-if wget $1 --no-check-certificate; then
-    echo "wget was successful"
-else
-    echo "wget failed"
-fi
+file = $(wget https://d37ci6vzurychx.cloudfront.net/trip-data/$1 --no-check-certificate)
+#    echo "wget was successful"
+ #   echo "file=$1."
+#else
+ #   echo "wget failed"
+#fi
+
+tar -xzf R413.tar.gz
+tar -xzf packages.tar.gz
+
+export PATH=$PWD/R/bin:$PATH
+export RHOME=$PWD/R
+export R_LIBS=$PWD/packages
+
+Rscript proj.R $file
